53fa66e61c4afde22752718a406c91aa0c78f566
KeMura-ops <kenti432@yahoo.co.jp>
2021-09-22 01:34:51 +0900

    補足

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import axios from 'axios';
 
 Vue.config.productionTip = false
 
+// main.jsにて「axios.defaults」とすることで、axiosのデフォルトの値を設定できる
 axios.defaults.baseURL = "https://firestore.googleapis.com/v1/projects/vuejs-http-c2a1a/databases/(default)/documents"
 
 new Vue({
