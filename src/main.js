4f5453ca9e14586e6241ea0a36136d521716ec8a
KeMura-ops <kenti432@yahoo.co.jp>
2021-09-22 18:27:00 +0900

    interceptorsについて

--- src/main.js
+++ src/main.js
@@ -7,6 +7,25 @@ Vue.config.productionTip = false
 // main.jsにて「axios.defaults」とすることで、axiosのデフォルトの値を設定できる
 axios.defaults.baseURL = "https://firestore.googleapis.com/v1/projects/vuejs-http-c2a1a/databases/(default)/documents"
 
+// データをサーバーに送る前に行いたい処理
+axios.interceptors.request.use(
+  config => {
+    return config;
+  },
+  error => {
+    return Promise.reject(error);
+  }
+);
+// サーバーから送られてきたデータを渡す前に行いたい処理
+axios.interceptors.response.use(
+  response => {
+    return response;
+  },
+  error => {
+    return Promise.reject(error);
+  }
+);
+
 new Vue({
   render: h => h(App),
 }).$mount('#app')
