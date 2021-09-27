6b81eac0d26827442afa4650fb6951dd9d4d53c3
KeMura-ops <kenti432@yahoo.co.jp>
2021-09-27 23:48:17 +0900

    Router.jsの導入

---
+++ src/router.js
@@ -0,0 +1,15 @@
+import Vue from 'vue';
+import Router from 'vue-router';
+import Comments from './views/Comments.vue';
+
+Vue.use(Router);
+
+export default new Router({
+  mode: 'history',
+  routes: [
+    {
+      path: '/',
+      component: Comments
+    }
+  ]
+});
