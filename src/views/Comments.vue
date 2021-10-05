8ac784c481f7d4786e04c9653eeecca9e44433de
KeMura-ops <kenti432@yahoo.co.jp>
2021-10-05 13:51:33 +0900

    一度コミット

--- src/views/Comments.vue
+++ src/views/Comments.vue
@@ -27,8 +27,17 @@ export default {
       posts: []
     };
   },
+  computed: {
+    idToken() {
+      return this.$store.getters.idToken;
+    }
+  },
   created() {
-    axios.get("/comments").then(response => {
+    axios.get("/comments", {
+      headers: {
+        Authorization: `Bearer ${this.idToken}`
+      }
+    }).then(response => {
       this.posts = response.data.documents;
     });
   },
@@ -44,8 +53,13 @@ export default {
               stringValue: this.comment
             }
           }
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${this.idToken}`
+          }
         }
-      )
+      );
       this.name = "";
       this.comment = "";
     }
